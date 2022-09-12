e782634f71ab07d3d960585e3bd94c598ac8f16d
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-12 22:21:07 +0300

    task_041

---
+++ task_041/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Какому числу будет равно М?");
+int M = Convert.ToInt32(Console.ReadLine());
+int[]array = new int[M];
+ int count = 0;
+ for (int i = 0; i < M; i ++)
+ {
+ Console.WriteLine("введите число: ");
+ array[i]=Convert.ToInt32(Console.ReadLine());
+ if(array[i] > 0)
+ {count ++;}
+ }
+ Console.Clear();
+ Console.WriteLine($"{string.Join(", ",array)} -> {count}");
